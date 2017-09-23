d50da6fa0623cc7a5376fa0b9c81eb0eb62e47df
Jared Diehl <jareddiehl96@gmail.com>
2017-09-23 15:17:14 -0500

    Added show password button

--- main.py
+++ main.py
@@ -2,65 +2,43 @@ from flask import Flask, request, redirect, render_template
 import cgi
 
 app = Flask(__name__)
-
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
-# a list of movies that nobody should have to watch
-terrible_movies = [
-    "Gigli",
-    "Star Wars Episode 1: Attack of the Clones",
-    "Paul Blart: Mall Cop 2",
-    "Nine Lives",
-    "Starship Troopers"
-]
-
-movies = [ "Star Wars", "Minions", "Freaky Friday", "My Favorite Martian" ]
-
-@app.route("/crossoff", methods=['POST'])
-def crossoff_movie():
-    crossed_off_movie = request.form['crossed-off-movie']
-
-    if crossed_off_movie not in movies:
-        # the user tried to cross off a movie that isn't in their list,
-        # so we redirect back to the front page and tell them what went wrong
-        error = "'{0}' is not in your Watchlist, so you can't cross it off!".format(crossed_off_movie)
-
-        # redirect to homepage, and include error as a query parameter in the URL
-        return redirect("/?error=" + error)
-
-    # remove movie from watchlist
-    movies.remove(crossed_off_movie)
-
-    # if we didn't redirect by now, then all is well
-    return render_template('crossoff.html', crossed_off_movie=crossed_off_movie)
-
-@app.route("/add", methods=['POST'])
-def add_movie():
-    # look inside the request to figure out what the user typed
-    new_movie = request.form['new-movie']
-
-    # if the user typed nothing at all, redirect and tell them the error
-    if (not new_movie) or (new_movie.strip() == ""):
-        error = "Please specify the movie you want to add."
-        return redirect("/?error=" + error)
-
-    # if the user wants to add a terrible movie, redirect and tell them the error
-    if new_movie in terrible_movies:
-        error = "Trust me, you don't want to add '{0}' to your Watchlist".format(new_movie)
-        return redirect("/?error=" + error)
-
-    # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
-    new_movie_escaped = cgi.escape(new_movie, quote=True) # TODO why is this here if it's not being used?
-    # add movie to watchlist
-    movies.append(new_movie)
-
-    return render_template('add-confirmation.html', movie=new_movie)
-
+@app.route("/welcome", methods=['GET', 'POST'])
+def welcome():
+    username = request.args.get("username")
+    return render_template('welcome.html', username=username)
 
 @app.route("/")
 def index():
-    encoded_error = request.args.get("error")
-    return render_template('edit.html', watchlist=movies, error=encoded_error and cgi.escape(encoded_error, quote=True))
+    return render_template('signup.html')
+
+@app.route("/", methods=['POST'])
+def login():
+    username = cgi.escape(request.form['username'].strip())
+    password = cgi.escape(request.form['password'].strip())
+    verify = cgi.escape(request.form['verify'].strip())
+    email = cgi.escape(request.form['email'].strip())
+    username_error = ''
+    password_error = ''
+    verify_error = ''
+    email_error = ''
+
+    if (not username) or username == "" or " " in username or len(username) < 3 or len(username) > 20:
+        username_error = "That's not a valid username"
+
+    if (not password) or password == "" or " " in password or len(password) < 3 or len(password) > 20:
+        password_error = "That's not a valid password"
+
+    if ((not verify) or verify == "" or (password and verify != password)) and password:
+        verify_error = "Passwords don't match"
+
+    if email and not("@" in email and "." in email and len(email) > 3 and len(email) < 20):
+        email_error = "That's not a valid email"
+
+    if username_error == '' and password_error == '' and verify_error == '' and email_error == '':
+        return redirect("/welcome?username=" + username)
+
+    return render_template('signup.html', username=username, email=email, username_error=username_error, password_error=password_error, verify_error=verify_error, email_error=email_error)
 
 app.run()
-
